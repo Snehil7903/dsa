2ac73939788073d2d6bcbaa1133c85094c18a52d
Snehil <snehilraj9570@gmail.com>
2026-06-16 17:13:17 +0530

    Add DSU

---
+++ Graph/6DSU/4dynamicGraph.java
@@ -0,0 +1,25 @@
+// But Suppose Edges Arrive One By One
+
+// Example:
+
+// Initially:
+
+// 0   1   2   3
+
+// Then:
+
+// Add edge (0,1)
+
+// Add edge (2,3)
+
+// Add edge (1,2)
+
+// Query:
+// Are 0 and 3 connected?
+
+// Add edge (4,5)
+
+// Query:
+// Are 1 and 5 connected?
+
+// This is a Dynamic Graph
